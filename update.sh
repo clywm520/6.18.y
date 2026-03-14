7d071509c289fcc7ecf91ade5a1da8f0dc5038b1
Your Name <you@example.com>
2026-03-14 17:09:07 +0800

    Update Linux kernel from v6.18.17 to v6.18.18

--- update.sh
+++ update.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #git remote add stable https://mirrors.tuna.tsinghua.edu.cn/git/linux-stable.git
-git fetch stable --no-tags --depth 1 refs/tags/v6.18.16:refs/tags/v6.18.16 refs/tags/v6.18.17:refs/tags/v6.18.17
-git diff v6.18.16 v6.18.17 > update-to-6.18.17.patch
-git apply --check update-to-6.18.17.patch
-git apply update-to-6.18.17.patch
+git fetch stable --no-tags --depth 1 refs/tags/v6.18.17:refs/tags/v6.18.17 refs/tags/v6.18.18:refs/tags/v6.18.18
+git diff v6.18.17 v6.18.18 > update-to-6.18.18.patch
+git apply --check update-to-6.18.18.patch
+git apply update-to-6.18.18.patch
 git add .
-git commit -m "Update Linux kernel from v6.18.16 to v6.18.17"
+git commit -m "Update Linux kernel from v6.18.17 to v6.18.18"
 
 # 3. 推送
 git push origin main
