1d778f79a6dcbf2cee48273a06bd89b6286f825b
Your Name <you@example.com>
2026-06-02 13:59:29 +0800

    Update Linux kernel from v6.18.33 to v6.18.34

--- update.sh
+++ update.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #git remote add stable https://mirrors.tuna.tsinghua.edu.cn/git/linux-stable.git
-git fetch stable --no-tags --depth 1 refs/tags/v6.18.32:refs/tags/v6.18.32 refs/tags/v6.18.33:refs/tags/v6.18.33
-git diff v6.18.32 v6.18.33 > update-to-6.18.33.patch
-git apply --check update-to-6.18.33.patch
-git apply update-to-6.18.33.patch
+git fetch stable --no-tags --depth 1 refs/tags/v6.18.33:refs/tags/v6.18.33 refs/tags/v6.18.34:refs/tags/v6.18.34
+git diff v6.18.33 v6.18.34 > update-to-6.18.34.patch
+git apply --check update-to-6.18.34.patch
+git apply update-to-6.18.34.patch
 git add .
-git commit -m "Update Linux kernel from v6.18.32 to v6.18.33"
+git commit -m "Update Linux kernel from v6.18.33 to v6.18.34"
 
 # 3. 推送
 git push origin main
